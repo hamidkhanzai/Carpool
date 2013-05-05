9e841b16d150577733a3bae41f285564c8c30a5c
hamidkhanzai <hamid_khanzai@yahoo.com>
2013-05-05 16:30:57 +0200

    first Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carpool::Application.config.secret_token = '56653750d1318c84e9b47177fe5eaeba51965df6debb98553bc38b7a090fa0ced55950895871c52fa549b1bbbebff24b9d5255130937b2134a0b70b0e287af85'
