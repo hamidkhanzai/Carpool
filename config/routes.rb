13b180ea17eb9709e83c0729bd964b3ae2ffa465
hamidkhanzai <hamid_khanzai@yahoo.com>
2013-05-05 19:06:49 +0200

    model controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Carpool::Application.routes.draw do
+  resources  :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20130505145813_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name, :limit=> 40
+      t.string :last_name,  :limit=> 40
+      t.string :username, :limit => 30
+      t.string :password, :limit=> 20
+      t.integer  :nid_no
+      t.string :phone, :limit=>15
+      t.string :email, :limit=>50
+      t.integer :licenceID
+
+      t.timestamps
+    end
+  end
+end
