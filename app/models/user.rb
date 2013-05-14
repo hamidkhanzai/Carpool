f8ae523ce854baece70f98c69f30d55ecb8c3c5a
hamidkhanzai <hamid_khanzai@yahoo.com>
2013-05-15 00:22:56 +0200

    improve Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,14 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :first_name, :last_name,:username,:email,:password,:nid_no,:licence_no,:phone,:password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :first_name, :last_name,:username,:nid_no,:licence_no,:phone
+
+  validates_presence_of :first_name, :last_name,:username,:nid_no,:licence_no, :on=> :update
+  validates_uniqueness_of :username, :on => :update
+
+  validates :phone, :format => {:with => /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/}, :on => :update
+
 
 
   #require 'digest/sha1'
