4aecdf2a03421f98c66203a6191ca2a2b5f4f682
hamidkhanzai <hamid_khanzai@yahoo.com>
2013-05-29 01:52:54 +0200

    nothing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,12 +9,7 @@ class SearchController < ApplicationController
   def list
     travel_date="#{params[:travel_date]["(1i)"]}-#{params[:travel_date]["(2i)"]}-#{params[:travel_date]["(3i)"]}"
 
-
-
-
-    @routs = Rout.where("d_to= ? and d_from= ? and travel_date= ?",params[:d_to],params[:d_from],travel_date)
-
-
+    @routs = Rout.where("d_to= ? and d_from= ? and travel_date= ?",params[:d_to],params[:d_from],travel_date).paginate(:page => params[:page], :per_page => 5)
 
   end
 end
