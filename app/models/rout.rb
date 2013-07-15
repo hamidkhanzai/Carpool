c7b352a99d05230706a15a4f383baa5394d03599
Alizai <alizai.csf@hotmail.com>
2013-07-15 18:39:01 +0200

    New Commit

--- app/models/rout.rb
+++ app/models/rout.rb
@@ -1,6 +1,6 @@
 class Rout < ActiveRecord::Base
   attr_accessible :description, :d_from, :d_to, :price, :travel_date,:free_seats,:userId ,:car_id, :travel_t
-   validates_presence_of :d_from, :d_to, :price, :travel_date,:free_seats 
+   validates_presence_of :d_from, :d_to, :price, :free_seats 
    validates :travel_t, :format => {:with => /^\(?(\d{2})\)?[: ]?(\d{2})$/}, :on => :update
   belongs_to :user
   belongs_to :car
